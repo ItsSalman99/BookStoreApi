14d096c71dca2523aee13d0c2d4acadf94aedc3f
DESKTOP-MUJTGOT\Ibn-e-Yunus <salmanabbas985@gmail.com>
2021-09-21 10:18:43 +0300

    storing book..

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -16,4 +16,36 @@ class BookController extends Controller
 
     }
 
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'title' => 'required|string',
+            'author_id' => 'required|int',
+            'img' => 'required|image|mimes:png,jpg|max:2048',
+            'pub_date' => 'required|date',
+            'no_pages' => 'required|int',
+            'lang' => 'required|string'
+        ]);
+        
+        if (!$validator->fails()) {
+            if ($request->hasFile('img')) {
+                $filename = $request->img->getClientOriginalName();
+        
+                $request->img->storeAs('book_img', $filename, 'public');
+            }
+        }
+        
+        $book = Book::create([
+            'title' => $request->title,
+            'author_id' => $request->author_id,
+            'img' => $filename,
+            'pub_date' => $request->pub_date,
+            'no_pages' => $request->no_pages,
+            'lang' =>$request->lang
+        ]);
+
+        return response($book,201);
+
+    }
+
 }
