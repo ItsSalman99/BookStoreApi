a07ae409f63cd814adfcd9a66d75ca7d409bf124
DESKTOP-MUJTGOT\Ibn-e-Yunus <salmanabbas985@gmail.com>
2021-09-21 10:14:44 +0300

    routes updated for authors

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/authors', [AuthorController::class,'index']);
+Route::post('/author', [AuthorController::class,'store']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
